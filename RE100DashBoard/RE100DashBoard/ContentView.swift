fb8458c03fecc2c59a50bb10a11adb88d06d25d6
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-07-15 17:44:54 +0900

    [Add] Xcode Project file

---
+++ RE100DashBoard/RE100DashBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RE100DashBoard
+//
+//  Created by 한경준 on 2022/07/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RE100DashBoard/RE100DashBoard/RE100DashBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RE100DashBoardApp.swift
+//  RE100DashBoard
+//
+//  Created by 한경준 on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct RE100DashBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
