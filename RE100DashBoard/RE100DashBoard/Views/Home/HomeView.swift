e20844848872a6214e2ce886bc5bea5b988133fd
hwiwonK <gnldnjs2469@gmail.com>
2022-07-28 21:51:12 +0900

    [Feat] Create home view folder and file #1

---
+++ RE100DashBoard/RE100DashBoard/Views/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  RE100DashBoard
+//
+//  Created by Kimhwiwon on 2022/07/28.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("This is home view")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
